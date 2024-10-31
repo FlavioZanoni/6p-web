a248c470f46aee36389e996b20c7d85aaaf4eaa1
Ghenoo <lucasgheno600@gmail.com>
2024-10-31 14:06:29 -0300

    chore: created order in the api with types and index

---
+++ app/lib/api/order/order.ts
@@ -0,0 +1,49 @@
+import { makeApiRequest } from "../makeApiRequest"
+import { GetApiParams, Pagination } from "../types"
+import { MutateOrder, Order } from "./types"
+
+export const getOrders = async ({
+  offset,
+  limit,
+  filter,
+}: GetApiParams) => {
+  return makeApiRequest<Pagination<Order>>({
+    method: "get",
+    url: `orders?offset=${offset}&limit=${limit}${filter || ""}`,
+  })
+}
+
+export const getOrdersPaginationless = async (): Promise<Order[]> => {
+  return await makeApiRequest({
+    method: "get",
+    url: `orders/list`,
+  })
+}
+
+export const getOrderById = async (id: number) => {
+  return makeApiRequest<Order>({
+    method: "get",
+    url: `orders/${id}`,
+  })
+}
+
+export const mutateOrder = async ({
+  id,
+  data,
+}: {
+  id?: number
+  data: MutateOrder
+}) => {
+  if (id) {
+    return makeApiRequest({
+      method: "put",
+      url: `orders/${id}`,
+      data: data,
+    })
+  }
+  return makeApiRequest({
+    method: "post",
+    url: `orders`,
+    data: data,
+  })
+}
---
+++ app/lib/api/order/types.ts
@@ -0,0 +1,8 @@
+import { DefType } from "../types"
+
+export interface Order extends DefType {
+}
+
+export interface MutateOrder {
+
+}
